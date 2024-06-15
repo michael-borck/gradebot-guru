836105047a08fa9a6a001178b6deb948bd0a1bc6
Michael Borck <michael.borck@curtin.edu.au>
2024-06-15 10:31:04 +0800

    docs: add doctests to submission_loader.py

--- gradebotguru/submission_loader.py
+++ gradebotguru/submission_loader.py
@@ -1,7 +1,6 @@
 import os
 from typing import Dict, List
 
-
 def load_submissions(directory: str) -> Dict[str, str]:
     """
     Load and parse student submissions from a directory.
@@ -11,6 +10,23 @@ def load_submissions(directory: str) -> Dict[str, str]:
 
     Returns:
         Dict[str, str]: A dictionary where keys are filenames and values are the content of the submissions.
+
+    Examples:
+        >>> import tempfile
+        >>> import os
+        >>> with tempfile.TemporaryDirectory() as tmpdir:
+        ...     file1_path = os.path.join(tmpdir, 'submission1.txt')
+        ...     file2_path = os.path.join(tmpdir, 'submission2.txt')
+        ...     with open(file1_path, 'w', encoding='utf-8') as f1:
+        ...         _ = f1.write('Content of submission 1')
+        ...     with open(file2_path, 'w', encoding='utf-8') as f2:
+        ...         _ = f2.write('Content of submission 2')
+        ...     submissions = load_submissions(tmpdir)
+        ...     submissions == {
+        ...         'submission1.txt': 'Content of submission 1',
+        ...         'submission2.txt': 'Content of submission 2'
+        ...     }
+        True
     """
     submissions = {}
     for filename in os.listdir(directory):
